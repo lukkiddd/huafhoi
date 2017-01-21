466c0236749305de3ae4ec9edfe80dfe39475956
Phasathorn Suwansri <arbacuz@gmail.com>
2017-01-22 02:04:59 +0700

    update

--- app.py
+++ app.py
@@ -133,6 +133,7 @@ def send_message(recipient_id, message_text):
     headers = {
         "Content-Type": "application/json"
     }
+
     data = json.dumps({
         "recipient": {
             "id": recipient_id
@@ -140,6 +141,7 @@ def send_message(recipient_id, message_text):
         "message": {
             "text": message_text
         }
+    })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
