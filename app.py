f75039195dbcc1702cfb15f29674a9c0bec122c2
Phasathorn Suwansri <arbacuz@gmail.com>
2017-01-24 02:17:11 +0700

    update

--- app.py
+++ app.py
@@ -139,8 +139,10 @@ def webhook():
                         for item in items_array:
                             if len(el) % 4 == 0 or item['name'] == items_array[-1]['name']:
                                 if len(el) > 1:
+                                    print "send elements"
                                     send_elements(sender_id, el, next_page, item['type'])
                                 else:
+                                    print "send generic"
                                     send_generic(sender_id, el, next_page, item['type'])
                                 el = []
                                 break
