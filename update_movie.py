00356777e2e890bec7ff549cb4ab32c260ff779d
Phasathorn Suwansri <arbacuz@gmail.com>
2017-02-10 09:43:28 +0700

    update movie

--- update_movie.py
+++ update_movie.py
@@ -7,22 +7,23 @@ from firebase import Firebase
 
 def scrap_movie():
     movies = []
-    for page in range(1,3):
-        url = "https://www.mastermovie-hd.com/page/"+str(page)
+    for page in range(1,2):
+        url = "https://www.moviehd-master.com/page/" + str(page)
         r  = requests.get(url)
         data = r.text
         soup = BeautifulSoup(data)
         for movie in soup.find_all('div', {'class':'item'}):
-            imdb = movie.find('div',{'class','imdb'})
-            imdb = imdb.find('p').get_text() if imdb != None else "N/A"
-            imdb = re.sub("\s+","",imdb)
-            if "N/A" not in imdb:
+            sound = movie.find('span', {'class':'soundmovie'})
+            imdb = movie.find('span',{'class','imdb'}).get_text()
+            if sound != None and "N/A" not in imdb:
                 imdb = float(imdb)
+                link = movie.find('a')['href']
+                title = movie.find('h2').get_text()
+                sound = sound.get_text().replace(" ","")
+                image = movie.find('img')['src']
+                resolution = movie.find('span', {'class':'calidad2'}).get_text() if movie.find('span', {'class':'calidad2'}) != None else u"ไม่ระบุ"
                 if imdb > 5:
-                    content = movie.find('div', {'class','item-content'})
-                    title = content.find('a').get_text()
-                    image = movie.find('img')['data-lazy-original']
-                    link = movie.find('a')['href']
+                    title = ' '.join([title, resolution, sound])
                     movies.append({
                         "title": title.lower(),
                         "image": image,
