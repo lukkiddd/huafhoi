26cdce381dec0d59d1e6be6b80aa964b469941fb
Phasathorn Suwansri <arbacuz@gmail.com>
2017-02-10 08:56:02 +0700

    update movie

---
+++ update_movie.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import requests
+from os import system
+import re
+import time
+from firebase import Firebase
+
+def scrap_movie():
+    movies = []
+    for page in range(1,3):
+        url = "https://www.mastermovie-hd.com/page/"+str(page)
+        r  = requests.get(url)
+        data = r.text
+        soup = BeautifulSoup(data)
+        for movie in soup.find_all('div', {'class':'item'}):
+            imdb = movie.find('div',{'class','imdb'})
+            imdb = imdb.find('p').get_text() if imdb != None else "N/A"
+            imdb = re.sub("\s+","",imdb)
+            if "N/A" not in imdb:
+                imdb = float(imdb)
+                if imdb > 5:
+                    content = movie.find('div', {'class','item-content'})
+                    title = content.find('a').get_text()
+                    image = movie.find('img')['data-lazy-original']
+                    movies.append({
+                        "title": title.lower(),
+                        "image": image,
+                        "imdb": imdb
+                    })
+    return movies
+
+def clear_firebase():
+	a = f.get()
+	if a != None:
+		for i in a:
+			q = Firebase('https://welse-141512.firebaseio.com/movies'+'/'+i)
+			q.remove()
+
+f = Firebase('https://welse-141512.firebaseio.com/movies');
+clear_firebase()
+movies = scrap_movie()
+f.set(movies)
+
--- wit_text.py
+++ wit_text.py
@@ -3,6 +3,17 @@ from wit import Wit
 
 access_token = "ELQM7CPEYW2YP6ODFKCCC7QLV6PE4WWZ"
 
+def first_entity_value(entities, entity):
+    """
+    Returns first entity value
+    """
+    if entity not in entities:
+        return None
+    val = entities[entity][0]['value']
+    if not val:
+        return None
+    return val['value'] if isinstance(val, dict) else val
+
 def send(request, response):
     print(response['text'])
 
